d72e04fc08bd1b376bc1c9e382f275a563455a92
Sergio Medina <hola@sergiomedina.io>
2023-04-20 11:55:54 -0300

    feat: add 404 page

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
     $vista = $_GET['seccion'];
     $vista = $vista ? $vista : 'home';
+    $existe = file_exists("views/$vista.php");
+    $vista_final = $existe ? $vista : '404';
 ?>
 
+<script>
+    var seccion = '<?= $vista ?>';
+    console.log("seccion: " + seccion);
+    console.log("existe: " + `<?php echo var_dump($existe) ?>`);
+</script>
+
 <?php include('partials/header.php') ?>
-<?php include ("views/$vista.php") ?>
+<?php include ("views/$vista_final.php") ?>
 <?php include('partials/footer.php') ?>
---
+++ views/404.php
@@ -0,0 +1,3 @@
+<h1>Página no encontrada</h1>
+<p>La página que estás buscando no existe.</p>
+<img src="img/404.png" alt="404" />
