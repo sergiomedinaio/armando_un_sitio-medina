4f483141d49142b7c5fa5836d26d6e9563d30aa0
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-12 11:40:19 -0300

    feat: add admin route (#1)
    * feat: add admin route
    
    * feat: add basic html structure

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin de mi Tiendita</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Admin de mi Tiendita</h1>
+    <p>Esto es el admin de mi tiendita</p>
+</body>
