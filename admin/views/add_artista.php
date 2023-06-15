955fbf167d091d9c9bc85be8ed0258311d9356d4
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-15 12:37:58 -0300

    feat: add form input fields

--- admin/views/add_artista.php
+++ admin/views/add_artista.php
@@ -5,6 +5,14 @@
     <div class="col-12">
         <h1 class="text-center my-5">Agregar Artista</h1>
         <form class="row" action="" method="post">
+            <div class="mb-3">
+                <label for="nombre_completo" class="form-label">Nombre completo</label>
+                <input class="form-control" id="nombre_completo" placeholder="">
+            </div>
+            <div class="mb-3">
+                <label for="foto_perfil" class="form-label">Foto perfil</label>
+                <input class="form-control" id="foto_perfil" placeholder="">
+            </div>
 
         </form>
     </div>
