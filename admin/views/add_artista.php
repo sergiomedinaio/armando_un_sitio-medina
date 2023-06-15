0790b3b77dd46c76935e9e8a068462604381959b
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-15 12:46:45 -0300

    feat: add submit input

--- admin/views/add_artista.php
+++ admin/views/add_artista.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-12">
         <h1 class="text-center my-5">Agregar Artista</h1>
-        <form class="row" action="" method="post">
+        <form class="row" action="actions/add_artista.php" method="post">
             <div class="mb-3">
                 <label for="nombre_completo" class="form-label">Nombre completo</label>
                 <input class="form-control" id="nombre_completo" placeholder="">
@@ -17,7 +17,9 @@
                 <label for="description" class="form-label">Biografia</label>
                 <textarea class="form-control" id="description" rows="3"></textarea>
             </div>
-
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Agregar artista</button>
+            </div>
         </form>
     </div>
 </div>
