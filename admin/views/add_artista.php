4846e20e6ecd92048fda7cb0ba192059006da241
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-15 12:29:24 -0300

    feat: add artista form

---
+++ admin/views/add_artista.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<div class="row">
+    <div class="col-12">
+        <h1 class="text-center my-5">Agregar Artista</h1>
+        <form class="row" action="" method="post">
+
+        </form>
+    </div>
+</div>
